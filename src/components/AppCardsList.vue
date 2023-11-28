263668630ce5f59c43b8dbeee2d35ad3462aea8b
ItsKristian01 <kristianhana11@gmail.com>
2023-11-28 16:33:56 +0100

    added input type select

--- src/components/AppCardsList.vue
+++ src/components/AppCardsList.vue
@@ -11,22 +11,38 @@ export default {
             store,
         };
     },
-    components: { AppLoader, AppCard},
+    components: { AppLoader, AppCard },
 };
 </script>
 
 <template>
-    <div class="container text-center">
-        <div class="row row-cols-5 g-4 p-3 mt-5">
+
+    <div class="input-group mb-3 mt-3 container-sm">
+        <select class="form-select" id="inputGroupSelect02">
+            <option selected>Choose...</option>
+            <option value="1">Alien</option>
+            <option value="2">Ally of Justice</option>
+            <option value="3">Ancient Gear</option>
+        </select>
+        <label class="input-group-text" for="inputGroupSelect02">Options</label>
+    </div>
+    
+    <div class="container">
+
+        <h5 class="badge text-bg-dark p-3 d-flex">FOUND number CARDS</h5>
+
+        <div class="row row-cols-5 g-4 p-3">
             <div class="col" v-for="cardsInfo in store.cards">
-                <AppCard :cardsInfo="cardsInfo"/>
+                <AppCard :cardsInfo="cardsInfo" />
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
 .container {
+    padding: 1rem;
     background-color: white;
 }
 </style>
