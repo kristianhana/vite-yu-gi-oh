0a42cee510eb286ac05f7148181929d86f23cf22
ItsKristian01 <kristianhana11@gmail.com>
2023-11-28 17:35:27 +0100

    added total number of cards

--- src/components/AppCardsList.vue
+++ src/components/AppCardsList.vue
@@ -20,7 +20,7 @@ export default {
 
         <AppLoader v-if="store.loading"/>
 
-        <h5 class="badge text-bg-dark p-3 d-flex">FOUND number CARDS</h5>
+        <h5 class="badge text-bg-dark p-3 d-flex">FOUND {{store.cards.length}} CARDS</h5>
 
         <div class="row row-cols-5 g-4 p-3">
             <div class="col" v-for="cardsInfo in store.cards">
